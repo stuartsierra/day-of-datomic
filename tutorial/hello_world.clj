9af414b78440d2cf63fa094042ecc8617fb2b5bd
Stuart Sierra <mail@stuartsierra.com>
2013-05-16 16:53:09 +1000

    hello_world: add more steps, ask for attr before 'touch'

--- tutorial/hello_world.clj
+++ tutorial/hello_world.clj
@@ -4,23 +4,24 @@
 (def conn (d/connect uri))
 
 ;; transaction input is data
-(def tx-result
-  (d/transact
-   conn
-   [[:db/add
-     (d/tempid :db.part/user)
-     :db/doc
-     "Hello world"]]))
+(def tx
+  [[:db/add
+    (d/tempid :db.part/user)  ; entity
+    :db/doc                   ; attribute
+    "Hello world"]])          ; value
 
 ;; transaction result is data
+(def tx-result (d/transact conn tx))
+
 tx-result
 
 (def dbval (d/db conn))
 
 ;; query input is data
-(def q-result (d/q '[:find ?e
-                     :where [?e :db/doc "Hello world"]]
-                   dbval))
+(def q-result
+  (d/q '[:find ?e
+         :where [?e :db/doc "Hello world"]]
+       dbval))
 
 ;; query result is data
 q-result
@@ -28,10 +29,11 @@ q-result
 ;; entity is a navigable view over data
 (def ent (d/entity dbval (ffirst q-result)))
 
-;; entities are lazy, so...
-(d/touch ent)
+;; entities are lazy until we ask
+(:db/doc ent)
 
 ;; schema itself is data
 (def doc-ent (d/entity dbval :db/doc))
 
+;; force loading of all attributes
 (d/touch doc-ent)
