7aa5e1852029b009fe362babb6b91995b801aaab
Stuart Sierra <mail@stuartsierra.com>
2013-09-18 09:21:05 -0500

    bootstrap script: creates clojure run scripts with lein classpath
    `lein classpath` returns absolute paths for dependencies, so the
    bootstrap script will have to be run *after* downloading a copy of
    this repository.

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -4,13 +4,10 @@ cd "$(dirname "$0")"/..
 
 lein deps
 
-# No idea how old Clojure versions sneak in...
-for ver in 1.2 1.3 1.4; do
-    rm -rf repository/org/clojure/clojure/${ver}*
-done
+lein classpath > .lein-classpath
 
-WINDOWS_JARS=`find repository -type f -name '*.jar' -print0 | tr '\000' ';'`
-UNIX_JARS=`find repository -type f -name '*.jar' -print0 | tr '\000' ':'`
+WINDOWS_JARS=`sed -e 's/:/;/g' < .lein-classpath`
+UNIX_JARS=`cat .lein-classpath`
 sed -e "s|REPLACE_WITH_WINDOWS_JARS|$WINDOWS_JARS|" \
     -e "s|REPLACE_WITH_UNIX_JARS|$UNIX_JARS|" \
     script/clojure.sh.template > clojure.sh
